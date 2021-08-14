691a49755cb0c14f8f8a012f8becc869f6641461
iasGitHub <idrisssall@yahoo.fr>
2021-08-14 12:01:20 +0000

    "modifier le nom de la variable entityManager en em "

--- src/Controller/PointSurveillanceController.php
+++ src/Controller/PointSurveillanceController.php
@@ -14,24 +14,24 @@ class PointSurveillanceController extends AbstractController
     #[Route('/point_surveillance', name :"point_surveillance")]
     public function index(): Response
     {
-            $entityManager = $this->getDoctrine()->getManager();
-            $data ["listeDesPoints"] = $entityManager->getRepository(PointSurveillance::class)->findAll();
-            $data ["listeZone"] = $entityManager->getRepository(Zone::class)->findAll();
+            $em = $this->getDoctrine()->getManager();
+            $data ["listeDesPoints"] = $em->getRepository(PointSurveillance::class)->findAll();
+            $data ["listeZone"] = $em->getRepository(Zone::class)->findAll();
             return $this->render('point_surveillance/index.html.twig', $data);
     }
 
     #[Route('/point_surveillance/ajoutPtS', name :"insertPtSurv")]
     public function addPointSurv(): Response
     {
-        $entityManager = $this->getDoctrine()->getManager();
+        $em = $this->getDoctrine()->getManager();
         $point = new PointSurveillance();
         $point->setCode($_POST["codePt"]);
         $point->setCoordonnees($_POST["coord"]);
         $point->setZones($_POST["zones"]);
-        $zone = $entityManager->getRepository(Zone::class)->find($_POST["selectZone"]);
+        $zone = $em->getRepository(Zone::class)->find($_POST["selectZone"]);
         $point->setZones($zone);
-        $entityManager->persist($point);
-        $entityManager->flush();
+        $em->persist($point);
+        $em->flush();
         return $this->redirectToRoute("point_surveillance");
     }
 
@@ -41,7 +41,7 @@ class PointSurveillanceController extends AbstractController
         public function create()
         {
             $point = new PointSurveillance();
-            $entityManager = $this->getDoctrine()->getManager();
+            $em = $this->getDoctrine()->getManager();
 
             return $this->redirectToRoute("point_surveillance");
         }
